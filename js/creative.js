9deee3f101c9a74c0ff4f60c1782efab158032dd
Marc Cooperstein <mcooperstein@gmail.com>
2016-07-29 19:38:13 -0700

    consistent double quotes
    consistent double quotes

--- js/creative.js
+++ js/creative.js
@@ -8,35 +8,35 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function (event) {
+    $("a.page-scroll").bind("click", function (event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        $("html, body").stop().animate({
+            scrollTop: ($($anchor.attr("href")).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
+    $("body").scrollspy({
+        target: ".navbar-fixed-top",
         offset: 51
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () {
-        $('.navbar-toggle:visible').click();
+    $(".navbar-collapse ul li a:not(.dropdown-toggle)").click(function () {
+        $(".navbar-toggle:visible").click();
     });
 
     // Fit Text Plugin for Main Header
     /*$("h1").fitText(
         1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
+            minFontSize: "35px",
+            maxFontSize: "65px"
         }
     );*/
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
+    $("#mainNav").affix({
         offset: {
             top: 100
         }
@@ -44,27 +44,27 @@
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
+    sr.reveal(".sr-icons", {
         duration: 600,
         scale: 0.3,
-        distance: '0px'
+        distance: "0px"
     }, 200);
-    sr.reveal('.sr-button', {
+    sr.reveal(".sr-button", {
         duration: 1000,
         delay: 200
     });
-    sr.reveal('.sr-contact', {
+    sr.reveal(".sr-contact", {
         duration: 600,
         scale: 0.3,
-        distance: '0px'
+        distance: "0px"
     }, 300);
 
     // Initialize and Configure Magnific Popup Lightbox Plugin
-    /*$('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
+    /*$(".popup-gallery").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
         gallery: {
             enabled: true,
             navigateByImgClick: true,
